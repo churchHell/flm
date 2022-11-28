3ff4f83b761294996c80a4baefd0984240580160
Siarhei Aliakseyeu <e.onizuka@mail.ru>
2022-11-28 14:21:49 +0300

    .

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Item;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
 use Livewire\Component;
+use Filament\Tables\Contracts\HasTable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
-class Items extends Component
+class Items extends Component implements HasTable
 {
+
+    use InteractsWithTable;
+
+    protected function getTableQuery(): Builder|Relation
+    {
+        return Item::query();
+    }
+
+    protected function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('name'),
+        ];
+    }
+
     public function render()
     {
         return view('livewire.items');
